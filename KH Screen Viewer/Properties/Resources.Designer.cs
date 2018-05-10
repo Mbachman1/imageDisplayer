5d7f64d0c84a7ae6b2c381966280e4cbf2c5439f
matthew.r.bachman <matthew.r.bachman@gmail.com>
2018-05-10 09:17:28 -0400

    removed un-needed file

--- KH Screen Viewer/Properties/Resources.Designer.cs
+++ KH Screen Viewer/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace KH_Screen_Viewer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap BeUqOHO {
-            get {
-                object obj = ResourceManager.GetObject("BeUqOHO", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap download {
             get {
                 object obj = ResourceManager.GetObject("download", resourceCulture);
